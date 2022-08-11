7b94b65672b09ba52c8dd21aafe1731d161fd319
Alejandro Luis Figueroa Cevallos <figueroaaab07@gmail.com>
2022-08-11 09:54:19 -0700

    Completed Assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,29 @@
 class ApplicationController < Sinatra::Base
-
+  # Add this line to set the Content-Type header for all responses
+  set :default_content_type, 'application/json'
   # add routes
+  get '/bakeries' do
+    # get all the bakeries from the database
+    bakeries = Bakery.all
+    # send them back as a JSON array
+    bakeries.to_json
+  end
+
+  get '/bakeries/:id' do
+      # look up the bakery in the database using its ID
+      bakery = Bakery.find(params[:id])
+      # send a JSON-formatted response of the bakery data with its baked good nested
+      bakery.to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do
+    baked_goods = BakedGood.order(price: :desc)
+    baked_goods.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    most_expensive = BakedGood.order(price: :desc).first
+    most_expensive.to_json
+  end
 
 end
